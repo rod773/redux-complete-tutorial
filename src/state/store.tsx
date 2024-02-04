05dbe4b02dc99369e01431f46cb2159cd0ca7648
rod773 <104315743+rod773@users.noreply.github.com>
2024-02-04 13:05:26 +0100

    store tsx

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -1,1 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {},
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = ReturnType<typeof store.dispatch>;
