5eec8eadf21bb0cb83c1a318a467d97ba7ba7b06
rod773 <104315743+rod773@users.noreply.github.com>
2024-02-04 14:41:25 +0100

    incrementByAmount

--- src/state/counter/counterSlice.tsx
+++ src/state/counter/counterSlice.tsx
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 interface CounterState {
   value: number;
@@ -18,9 +18,13 @@ export const counterSlice = createSlice({
     decrement: (state) => {
       state.value -= 1;
     },
+
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
   },
 });
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 
 export default counterSlice.reducer;
