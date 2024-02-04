5e9dac735850fa823ac3536a93b5c2c21dd8e617
rod773 <104315743+rod773@users.noreply.github.com>
2024-02-04 14:24:17 +0100

    buttons

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,12 +1,31 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../state/store";
+import { decrement, increment } from "../state/counter/counterSlice";
 
 const Counter = () => {
   const count = useSelector((state: RootState) => state.counter.value);
 
   const dispatch = useDispatch();
 
-  return <div>Counter</div>;
+  return (
+    <div className="">
+      <h2>Count = {count}</h2>
+      <div className="d-flex justify-content-center container">
+        <button
+          onClick={() => dispatch(increment())}
+          className="btn btn-success mx-5"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => dispatch(decrement())}
+          className="btn btn-warning mx-5"
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Counter;
