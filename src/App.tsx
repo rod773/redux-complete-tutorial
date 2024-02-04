93e42f718875312e5300f6e794402f87967a4fa9
rod773 <104315743+rod773@users.noreply.github.com>
2024-02-04 12:40:39 +0100

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import "./App.scss";
+import Counter from "./components/Counter";
+
+//Store
+
+interface CounterState {
+  value: number;
+}
+
+interface UserState {
+  isSignedIn: boolean;
+}
+
+//Actions
+
+const increment = {
+  type: "INCREMENT",
+};
+
+const decrement = {
+  type: "DECREMENT",
+};
 
 function App() {
   return (
     <div>
       <h1>Redux Complete Tutorial</h1>
+      <Counter />
     </div>
   );
 }
